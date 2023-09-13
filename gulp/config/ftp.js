7b9947a4c198f5f99bbb91015836c381cf781a02
Medeu <medeuserikuly@gmail.com>
2023-09-13 18:07:40 +0600

    add favicon and metatags

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-    host: 'ernartoleubek.kz', // Адрес ФТП сервера
-    user: 'ernarto1', 
-    password: '38nd#Gnq6%9I',
+    // host: 'ernartoleubek.kz', // Адрес ФТП сервера
+    // user: 'ernarto1', 
+    // password: '38nd#Gnq6%9I',
     parallel: 5 // Кол-во одновременных потоков
 };
